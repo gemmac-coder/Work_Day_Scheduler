8717a23547cd7a5697c3d3cb34f27180a84fc3ed
gemmacollinge <gemmacollinge_71@hotmail.com>
2021-04-02 14:45:03 +0100

    added local storage so textarea content saves

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,28 +8,50 @@ const renderCalenderEvents = () => {
   const schedulerEvents = JSON.parse(localStorage.getItem("schedulerEvents"));
 
   if (schedulerEvents !== null) {
-    // const currentHour = moment().hour();
-    const currentHour = 12;
+    const currentHour = moment().hour();
     const timeBlocks = $(".container .row");
     console.log(timeBlocks);
     const callback = function () {
+      const textArea = $(this).find("textarea");
       const timeBlockTime = Number.parseInt($(this).data("time"), 10);
       if (timeBlockTime === currentHour) {
-        $(this).find("textarea").removeClass("past").addClass("present");
+        textArea.removeClass("past").addClass("present");
       }
       if (timeBlockTime > currentHour) {
-        $(this).find("textarea").removeClass("past").addClass("future");
+        textArea.removeClass("past").addClass("future");
       }
+      const scheduledEvent = schedulerEvents[timeBlockTime];
+      textArea.text(scheduledEvent);
     };
 
     timeBlocks.each(callback);
   } else {
-    localStorage.setItem("schedulerEvents", JSON.stringify({ 9: "hello" }));
+    localStorage.setItem("schedulerEvents", JSON.stringify({}));
+  }
+};
+
+const onClick = function (event) {
+  const schedulerEvents = JSON.parse(localStorage.getItem("schedulerEvents"));
+  const target = $(event.target);
+  const currentTarget = $(event.currentTarget);
+  if (target.is("button")) {
+    const key = target.attr("id");
+    const value = target.parent().find("textarea").val();
+
+    const newObject = {
+      ...schedulerEvents,
+      [key]: value,
+    };
+
+    localStorage.setItem("schedulerEvents", JSON.stringify(newObject));
+    console.log(key, value);
+    console.log(newObject);
   }
 };
 
 const onReady = () => {
   console.log("I am ready");
+  $(".container").click(onClick);
   renderCurrentDate();
 
   renderCalenderEvents();
